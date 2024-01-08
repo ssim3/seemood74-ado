87d2a6219cb80910ee41376bb8ea08e3097691e7
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2024-01-08 14:46:22 +0800

    Bug Fixes on several tests
    Fixed bugs found in positive tests, "NULL" dates and etc.

--- tests/test_raw_orderdetail_unitprice_quantity_discount_is_positive.sql
+++ tests/test_raw_orderdetail_unitprice_quantity_discount_is_positive.sql
@@ -4,4 +4,4 @@ SUM(quantity) AS total_quantity,
 SUM(discount) AS total_discount
 FROM {{ ref('raw_orderdetail' )}}
 GROUP BY OrderID, ProductID
-HAVING (total_price < 0) AND (total_quantity < 0) AND (discount < 0)
+HAVING (total_price < 0) AND (total_quantity < 0) AND (total_discount < 0)
