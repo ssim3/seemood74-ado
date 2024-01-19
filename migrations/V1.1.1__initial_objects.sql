09fad8fd92d4e0ff86c9ffc6ecda942514d26311
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2024-01-19 15:59:58 +0800

    Create V1.1.1__initial_objects.sql

---
+++ migrations/V1.1.1__initial_objects.sql
@@ -0,0 +1,10 @@
+CREATE TABLE PRODUCT_FRESH
+(
+   ProductID INT PRIMARY KEY,
+   ProductName VARCHAR(40),
+   ListPrice DECIMAL(10, 4),
+   UnitCost DECIMAL(10, 4),
+   Discontinued BOOLEAN,
+   CategoryName VARCHAR(15),
+   Supplier VARCHAR(40)
+);
