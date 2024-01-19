79af48fe81643e769053c9062c499224affcee20
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2024-01-19 17:32:01 +0800

    Update V1.1.3__merge_objects.sql

--- migrations/V1.1.3__merge_objects.sql
+++ migrations/V1.1.3__merge_objects.sql
@@ -1,4 +1,17 @@
 CREATE OR REPLACE TABLE RAW_PRODUCT AS
-SELECT *
-FROM RAW_PRODUCT INNER JOIN PRODUCT_FRESH 
+SELECT 
+r.ProductID AS ProductID,
+r.ProductName AS ProductName,
+r.SupplierID AS SupplierID,
+r.CategoryID AS CategoryID,
+r.QuantityPerUnit AS QuantityPerUnit,
+r.UnitPrice AS UnitPrice,
+r.UnitsInStock AS UnitsInStock,
+r.UnitsOnOrder AS UnitsOnOrder,
+r.ReorderLevel AS ReorderLevel,
+r.Discontinued AS Discontinued,
+f.UnitCost AS UnitCost,
+f.CategoryName AS CategoryName,
+f.Supplier AS SupplierName
+FROM RAW_PRODUCT r INNER JOIN PRODUCT_FRESH f 
 ON RAW_PRODUCT.ProductID = PRODUCT_FRESH.ProductID;
