fefe093469dfc576f5fddba2b64b7665c89c06df
ian choy <s10243089@connect.np.edu.sg>
2024-02-09 12:14:11 +0800

    Documented programme
    Details

--- models/transformation/V1.1.1__remove_region_customer.sql
+++ models/transformation/V1.1.1__remove_region_customer.sql
@@ -1,9 +1,13 @@
 -- Dropping region columns in tables
+
+-- Dropping Region from CUSTOMER Table
 ALTER TABLE RAW_CUSTOMER
 DROP COLUMN region;
 
+-- Dropping Region from EMPLOYEE Table
 ALTER TABLE RAW_EMPLOYEE
 DROP COLUMN Region;
 
+-- Dropping shipRegion from ORDER Table
 ALTER TABLE RAW_ORDER
 DROP COLUMN shipRegion;
